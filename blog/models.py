51ac79b67ea8debb56240a95a46dda2806a87ceb
JeffChen <jeffchen1118@gmail.com>
2024-12-21 18:00:34 +0000

    Added Comment model

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.author)
