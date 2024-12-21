4441387598e6509f6dc1ec580a5e245d20e7a861
JeffChen <jeffchen1118@gmail.com>
2024-12-21 07:47:30 +0000

    update blog model.py

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = (
     (0, "Draft"),
-    (1, "Publish")
+    (1, "Published")
 )
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -12,7 +12,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
