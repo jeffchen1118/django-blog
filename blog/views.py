a6f7bbdb27c63f77fe90b8a66d071fc2ee20d062
JeffChen <jeffchen1118@gmail.com>
2025-01-06 14:17:05 +0000

    Added Comment update and delete function

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse 
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 # Create your views here.
 class PostList(generic.ListView):
@@ -54,3 +55,68 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    Edit an individual :model:`blog.Comment`.
+
+    **Context**
+
+    ``comment``
+        An instance of :model:`blog.Comment`.
+
+    **Template:**
+
+    :template:`blog/comment_edit.html`
+    """
+    if request.method == "POST":
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, id=comment_id)
+        comment_form = CommentForm(request.POST, instance=comment)
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Comment Updated!'
+            )
+        else:
+            messages.add_message(
+            request, messages.ERROR,
+            'Error updating comment, please try again'
+            )         
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual :model:`blog.Comment`.
+
+    **Context**
+
+    ``comment``
+        An instance of :model:`blog.Comment`.
+
+    **Template:**
+
+    :template:`blog/comment_delete.html`
+    """
+    
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment Deleted!'
+        )
+    else:
+        messages.add_message(
+        request, messages.ERROR,
+        'Error deleting comment, please try again'
+        )         
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+   
