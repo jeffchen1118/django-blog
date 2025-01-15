a450202e6353a9e175af371c64766d79f7301160
JeffChen <jeffchen1118@gmail.com>
2025-01-15 11:01:42 +0000

    test summernote in about

--- about/forms.py
+++ about/forms.py
@@ -1,11 +1,15 @@
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 from .models import CollaborateRequest
         
 class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
-        fields = (
+        fields = [
                     'name',
                     'email',
                     'message',
-        )
+        ]
+        widgets = {
+            'message': SummernoteWidget(),
+        }
